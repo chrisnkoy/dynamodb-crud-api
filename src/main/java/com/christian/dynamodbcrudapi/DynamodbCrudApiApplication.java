c8654769ae7483cdefc055544e6aff9d3958308e
Christian Nkoy <nkoycm@gmail.com>
2023-08-05 12:24:14 -0400

    First commit

---
+++ src/main/java/com/christian/dynamodbcrudapi/DynamodbCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.christian.dynamodbcrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamodbCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamodbCrudApiApplication.class, args);
+	}
+
+}
